1b32ca4075dd1c335d3c184b6a9ea48b21119227
JuliavanDriel <31695724+JuliavanDriel@users.noreply.github.com>
2022-01-19 11:39:47 +0100

    Update file.js

--- frontend/src/components/file.js
+++ frontend/src/components/file.js
@@ -3,6 +3,5 @@ require("chromedriver");
 async function example(){
 let driver = await new Builder().forBrowser("chrome").build();
 await driver.get("http://localhost:3000/");
-await driver.findElement(By.name("TitleI")).sendKeys("Login...", Key.RETURN);}
 example();
 
