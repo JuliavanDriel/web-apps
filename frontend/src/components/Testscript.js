8ece7a42d6495e253c6e98ace723be68b81cc623
JuliavanDriel <31695724+JuliavanDriel@users.noreply.github.com>
2022-01-27 16:43:11 +0100

    Update Testscript.js

--- frontend/src/components/Testscript.js
+++ frontend/src/components/Testscript.js
@@ -3,6 +3,7 @@ require("hromedriver");
 async function example(){
 let driver = await new Builder().forBrowser("chrome").build();
 await driver.get("http://localhost:3000/vragenlijst");
-await driver.findElement(By.name("TitleQ")).sendKeys("Testen. Testen. Testen.", Key.RETURN);}
+await driver.findElement(By.name("TitleQ")).sendKeys("Testen. Testen. Testen.", Key.RETURN);
+await driver.quit();
+}
 example();
-await driver.quit()
