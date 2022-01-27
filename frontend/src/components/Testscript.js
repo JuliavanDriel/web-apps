bdf47aafdddf27cf5434d1e756b503ee5f2ec51e
JuliavanDriel <31695724+JuliavanDriel@users.noreply.github.com>
2022-01-27 16:45:13 +0100

    Update Testscript.js

--- frontend/src/components/Testscript.js
+++ frontend/src/components/Testscript.js
@@ -1,9 +1,11 @@
 const {Builder, By, Key, util} = require("selenium-webdriver");
-require("hromedriver");
+require("chromedriver");
 async function example(){
 let driver = await new Builder().forBrowser("chrome").build();
 await driver.get("http://localhost:3000/vragenlijst");
-await driver.findElement(By.name("TitleQ")).sendKeys("Testen. Testen. Testen.", Key.RETURN);
+let data = await driver.findElement(By.id("id")).getText();
+await driver.findElement(By.id("idknop")).click();
+console.log(Result is:+ data);
 await driver.quit();
-}
+ }
 example();
